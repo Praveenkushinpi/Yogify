e1c80ed7a297f7dbb574901a124efe395bc4b4c7
Praveen kumar <168343716+Praveenkushinpi@users.noreply.github.com>
2025-05-18 11:33:51 +0530

    navBar and layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
-
+import BackgroundImg from './components/backGround';
 export default function Page() {
   return (
-   <View className=''
+   <View className="bg-[url(assets/images/assets/images/luke-richardson-dI7vfR1Bqcg-unsplash.jpg)] ">
+<BackgroundImg />
+   </View>
   );
 }
